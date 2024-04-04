525f87733a62b73883b8ce46521ae258bb36dbfb
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-04 09:55:46 -0400

    My work

---
+++ Classes/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Classes
+{
+	public class car
+	{
+		public car()
+		{
+		}
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2020;
+
+            Car myCar1 = new Car()
+            {
+                Make = "Tesla",
+                Model = "Model 3",
+                Year = 2019
+            };
+
+            Car myCar2 = new Car("BMW", "i5", 2025);
+
+            var carList = new List<Car>() { myCar, myCar1, myCar2 };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            }
+
         }
     }
 }
